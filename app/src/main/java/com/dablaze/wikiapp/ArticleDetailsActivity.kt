3dc6cd2585e23b55cccc6f032ec82d698d50c906
Dablaze-ufc <chukwuwauchenna@gmail.com>
2019-11-17 16:23:30 +0100

    Initial commit

---
+++ app/src/main/java/com/dablaze/wikiapp/ArticleDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.dablaze.wikiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticleDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_details)
+    }
+}
