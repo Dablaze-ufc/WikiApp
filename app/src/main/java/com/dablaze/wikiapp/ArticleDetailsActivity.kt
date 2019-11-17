3c6c23ce792c4e4667ccba4d8027ed238e36512c
Dablaze-ufc <chukwuwauchenna@gmail.com>
2019-11-17 18:56:34 +0100

    added tool bar to the explore fragment and some house keeping

--- app/src/main/java/com/dablaze/wikiapp/ArticleDetailsActivity.kt
+++ app/src/main/java/com/dablaze/wikiapp/ArticleDetailsActivity.kt
@@ -2,11 +2,23 @@ package com.dablaze.wikiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_article_details.*
 
 class ArticleDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_details)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == android.R.id.home) {
+            finish()
+        }
+        return true
     }
 }
